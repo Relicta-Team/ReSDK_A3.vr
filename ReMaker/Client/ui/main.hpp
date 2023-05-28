6301602492713d5509fea2fa37927a8b2aaf3221
Yodes <35444748+Yobas@users.noreply.github.com>
2023-05-28 15:34:19 +0300

    Initial ReMaker

---
+++ ReMaker/Client/ui/main.hpp
@@ -0,0 +1,6 @@
+class mainDisplay {
+	idd = 10000;
+	movingEnable = 0;
+	enableSimulation = 1;
+	name = "mainDisplay";	
+};
---
+++ ReMaker/Client/ui/titles.hpp
@@ -0,0 +1,6 @@
+class GUI
+{
+    idd = 10500;
+	duration = 60*60*24;
+    onLoad = "uiNamespace setVariable ['gui', _this select 0]";
+}
