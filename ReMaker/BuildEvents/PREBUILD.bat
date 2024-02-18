5ef1b94d11423d3b205ce033c8af662db09d2efc
Yodes <35444748+Yobas@users.noreply.github.com>
2024-02-18 16:55:53 +0300

    ReMaker 9.0

---
+++ ReMaker/BuildEvents/PREBUILD.bat
@@ -0,0 +1,18 @@
+echo COMMAND - [%1]
+
+set workdir=%cd%\..
+echo Work directory: %workdir%
+
+set renode_wd=%workdir%\ReNode
+set renode_exe=%renode_wd%\ReNode.exe
+
+echo RENODE: "%renode_exe%"
+
+if not exist "%renode_exe%" (
+	echo ReNode executable not found: %renode_exe%
+	exit /b -10201
+)
+
+start "renode_build" /d "%renode_wd%" /i /b /wait "%renode_exe%" -prep_code -nosplash -noapp
+
+exit /b %errorlevel%
