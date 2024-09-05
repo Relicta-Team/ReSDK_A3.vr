19590e8b23db7a263cfc58224fc62e73ce618d66
CopyrightBot <info@relicta.ru>
2024-09-05 01:35:18 +0000

    Update copyright

--- Src/Scripts/RBuilder.cs
+++ Src/Scripts/RBuilder.cs
@@ -1,3 +1,8 @@
+// ======================================================
+// Copyright (c) 2017-2024 the ReSDK_A3 project
+// sdk.relicta.ru
+// ======================================================
+
 using System;
 using ReEngine;
 using System.Threading;
