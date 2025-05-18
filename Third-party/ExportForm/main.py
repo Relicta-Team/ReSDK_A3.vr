c1d6e8922a431f448deb2c09e1125efdb6b95b89
Yodes <35444748+Yobas@users.noreply.github.com>
2025-05-18 23:56:34 +0300

    improvement export script

--- Third-party/ExportForm/main.py
+++ Third-party/ExportForm/main.py
@@ -77,13 +77,17 @@ print("CURRENT PREFIX: " + pboPrefix or "not set")
 print("\n"*3)
 
 modelsOut = []
+imagesOut = []
 
-with open(configFile,"r") as f:
+with open(configFile,"r",encoding="utf-8") as f:
 	data = f.read()
 	models = []
+	images = []
 	models += [m[0] for m in re.finditer(r"[\\/]?(\w+[\\/])*\w+\.p3d",data)]
 	models += [m[1] for m in re.finditer(r"model\s*\=\s*['\"]([\\/]?(\w+[\\/])*\w+(\.p3d)?)",data,re.IGNORECASE)]
 	
+	images += [m[0] for m in re.finditer(r"[\\/]?(\w+[\\/])*\w+\.paa",data,re.IGNORECASE)]
+
 	#lower
 	models = [m.lower() for m in models]
 	# add p3d if not setted
@@ -94,11 +98,30 @@ with open(configFile,"r") as f:
 	# fix paths
 	models = [m.replace("/","\\") for m in models]
 
+	#also for images
+	images = [m.lower() for m in images]
+	# add p3d if not setted
+	images = [m if m.endswith(".paa") else m + ".paa" for m in images]
+	# remove duplicates
+	images = list(set(images))
+	
+	# fix paths
+	images = [m.replace("/","\\") for m in images]
+
+	# remove images starts with \a3\
+	images = [m for m in images if not m.startswith("\\a3\\")]
+
+	print(">>>> MODELS: ")
 	for m in models:
+		print(m)
+	print(">>>> IMAGES: ")
+	for m in images:
 		print(m)
 	
 	modelsOut = models
-print("\t\tCount: " + str(len(modelsOut)))
+	imagesOut = images
+
+print("\t\tCount: " + str(len(modelsOut)) + " models, " + str(len(imagesOut)) + " images")
 
 print ("\n"*3)
 print("=========== CLONING MODELS ============")
@@ -125,12 +148,16 @@ for m_withpref in modelsOut:
 	if not os.path.exists(absModelPath):
 		print(f'File {absModelPath} doesn\'t exist!')
 	else:
+		toFolderDir = os.path.dirname(os.path.join(toFolder, m))
+		if not os.path.exists(toFolderDir):
+			print("\t create dir: " + toFolderDir)
+			os.makedirs(toFolderDir)
 		print("copy: " + m)
 		shutil.copyfile(os.path.join(fromFolder, m), os.path.join(toFolder, m))
 
 print("\n"*3)
 print("=========== STARTING COLLECT DATA ============")
-dataPathes = []
+dataPathes = imagesOut
 for root, _, files in os.walk(toFolder):
 	for file in files:
 		if file.endswith(".p3d"):
@@ -143,8 +170,10 @@ for root, _, files in os.walk(toFolder):
 			
 			#open file as text and search all pathes with ending .paa
 			contents = getPboPathes(fpath,pboPrefix)
-			for c in contents:
-				print("\tfound:" + c)
+			
+			#for c in contents:
+				#print("\tfound:" + c)
+			print("\tfound " + str(len(contents)) + " pathes: " + (' ; '.join(contents)))
 
 			dataPathes += contents
 
@@ -156,10 +185,12 @@ print("\t\tCount: " + str(len(dataPathes)))
 # copy data files
 print("\n"*3)
 print("=========== CLONING DATA ============")
-bufferedPrefixes = []
+bufferedPrefixes:list[(str,str,str)] = [] #buffprefix,buffpathfrom,buffpathto
 for d in dataPathes:
 	curDirFrom = fromFolder
-	print("before check: " + d)
+	curDirTo = toFolder
+	#print("before check: " + d)
+
 	# first step - removing prefix if setted
 	if pboPrefix:
 		pfx = pboPrefix.lower()
@@ -167,10 +198,11 @@ for d in dataPathes:
 			
 			#search in buffered prefixes
 			override = False
-			for (bp,bp_path) in bufferedPrefixes:
+			for (bp,bp_path,bp_pathdest) in bufferedPrefixes:
 				if (bp + "\\") in d:
 					pfx = bp
 					curDirFrom = bp_path
+					curDirTo = bp_pathdest
 					print("override prefix found: " + pfx)
 					override = True
 					break
@@ -185,26 +217,38 @@ for d in dataPathes:
 					pfxnew = f.read()
 					pfxnew = pfxnew.replace("\n","")
 					pfxnew = pfxnew.replace("\r","")
+
+				r2 = input("Input dest dir for addon: ")
+				if r2 == "" or not os.path.exists(r2):
+					raise Exception(f'Dir not found: {r2}')
+
 				pfx = pfxnew.lower()
 				curDirFrom = res
-				print("Register new prefix: " + pfx + " ===> " + res)
-				bufferedPrefixes.append((pfx,res))
+				curDirTo = r2
+				print("Register new prefix: " + pfx + " ===> " + res + "(orig: " + r2 + ")")
+				bufferedPrefixes.append((pfx,res,r2))
 
 		d = d.replace(pfx,"")
 		d = d.lstrip("\\")
 
-	print("check: " + d)
+	#print("check: " + d)
 
 	if not os.path.exists(os.path.join(curDirFrom, d)):
 		raise Exception(f'ORIG ERROR: File {os.path.join(curDirFrom, d)} doesn\'t exist!')
 	if not os.path.isfile(os.path.join(curDirFrom, d)):
 		raise Exception(f'ORIG ERROR: File {os.path.join(curDirFrom, d)} is not a file!')
 	
-
+	toFolderDir = os.path.dirname(os.path.join(curDirTo, d))
+	if not os.path.exists(toFolderDir):
+		print("\t create dir: " + toFolderDir)
+		os.makedirs(toFolderDir)
 	print("copy: " + d)
-	#shutil.copyfile(os.path.join(fromFolder, d), os.path.join(toFolder, d))
+	shutil.copyfile(os.path.join(curDirFrom, d), os.path.join(curDirTo, d))
 
 
 print(f"Buffered prefixes ({len(bufferedPrefixes)}):")
-for (bp,bp_path) in bufferedPrefixes:
-	print("\t\t" + bp + " => " + bp_path)
+for (bp,bp_path,bp_pathdest) in bufferedPrefixes:
+	print("\t\t" + bp + " => " + bp_path + " => " + bp_pathdest)
+
+print("\n"*3)
+print("=========== FINISHED ============")
